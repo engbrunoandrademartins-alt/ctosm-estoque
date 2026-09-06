d0e4ae4e59eeaf774fcd94cdb9cca2c3291ef2de
engbrunoandrademartins-alt <engbrunoandrademartins@gmail.com>
2026-09-06 09:06:34 -0400

    Fix indentation in supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CTOSM_SUPABASE_CONFIG = {
-  url: 'https://cqopqgmsjwfczhbiishj.supabase.co',
+     url: 'https://cqopgqmsjwfczhbiishj.supabase.co',
   key: 'sb_publishable_IAtEhn366TvslxurRLPfyA_zJ625Slv'
 };
